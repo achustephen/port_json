8c0b6095a1ded510a9654fa1d81b377fa26a9043
achustephen <achustephen182@gmail.com>
2026-04-24 17:57:22 +0530

    ..

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const output = document.getElementById("output");
+let files=[];   
 function createNode(node) {
   const element = document.createElement("div");
 
-  if (node.type === "folder") {
+  if (node.type === "folder"){
     element.classList.add("folder");
     const header=document.createElement("div");
     header.classList.add("folder-header");
     header.textContent = "📁" + node.name;
-    element.appendChild(header);
-        
+     element.appendChild(header);
+    // let head=header.textContent;
+    // files.push(head);
       const childrenContainer = document.createElement("div");
       childrenContainer.classList.add("children","hidden");
       if (node.children) {
@@ -27,16 +29,15 @@ function createNode(node) {
         header.textContent = "\u2191" + "📁" + node.name;
       }
     });
-
   } 
   else {
     element.classList.add("file");
     element.textContent = "📄" + node.name;
+    // let elem=element.textContent;
+    // files.push(elem);
   }
-
   return element;
 }
-
 function generateTree(){
   const input = document.getElementById("jsonInput").value;
   output.innerHTML = "";
@@ -48,7 +49,6 @@ function generateTree(){
     alert("Invalid JSON!");
   }
 }
-
 const egJson=`{
   "name": "Portfolio",
   "type": "folder",
@@ -73,15 +73,31 @@ const egJson=`{
     }
   ]
 }`;
-
 const textarea = document.getElementById("jsonInput");
-
 textarea.value = egJson;
-
 function clearEg() {
   textarea.value = "";
   output.innerHTML= "";
 }
+function debounce(){
+  let timer;
+  document.getElementById("searchInput").addEventListener("input", () => {
+  clearTimeout(timer);
+  timer = setTimeout(searchItem, 300);
+});}
 
-
-
+function searchItem(){
+  const search=document.getElementById("searchInput").value.toLowerCase();
+  const result=document.getElementById("result");
+  result.innerHTML= "";
+  const ul=document.createElement("ul");
+  files.forEach(file => {
+    if(file.toLowerCase().includes(search))
+    {
+      const li=document.createElement("li");
+      li.textContent=file;
+      console.log(li);
+      ul.appendChild(li);
+    }
+  });
+}
